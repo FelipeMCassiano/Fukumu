17a77c5f7cb70eb7cbf10c2797bd5beb35197385
FelipeMCassiano <felipemcassiano@gmail.com>
2024-07-06 02:30:53 -0300

    feat: uses a 'image' instead of a local

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FelipeMCassiano/Fukumu
 
 go 1.22.4
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"log"
 	"os"
@@ -8,7 +9,13 @@ import (
 	"path/filepath"
 	"strconv"
 	"syscall"
+
+	"github.com/google/uuid"
 )
+
+func init() {
+	checkErr(os.MkdirAll("./containers", 0700), "init containers dir")
+}
 
 func main() {
 	command := os.Args[1]
@@ -37,20 +44,30 @@ func run() {
 	checkErr(cmd.Run(), "run run()")
 }
 
+// go:embed ubuntu-base-20.04.1-base-amd64.tar.gz
+var f embed.FS
+
 func child() {
 	fmt.Printf("Running %v as %d\n", os.Args[2], os.Getpid())
 
 	cg()
 
+	uid, err := uuid.NewRandom()
+	checkErr(err, "child(): uuid")
+
+	containerId := "f-" + uid.String()
+
+	rootfsDir := filepath.Join("./containers", containerId)
+
 	syscall.Sethostname([]byte("container"))
-	syscall.Chroot("/")
-	syscall.Chdir("/")
+	unzipImage(rootfsDir, "./ubuntu-base-20.04.1-base-amd64.tar.gz")
 	syscall.Mount("proc", "proc", "proc", 0, "")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	pivotRoot(rootfsDir)
 
 	checkErr(cmd.Run(), "run child()")
 	syscall.Unmount("/proc", 0)
@@ -72,6 +89,23 @@ func cg() {
 	// Write configuration files for cgroup v2
 	checkErr(os.WriteFile(filepath.Join(fukumu, "pids.max"), []byte("20"), 0700), "write pids.max")
 	checkErr(os.WriteFile(filepath.Join(fukumu, "cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700), "write cgroup.procs")
+}
+
+func pivotRoot(newRoot string) {
+	checkErr(syscall.Mount(newRoot, newRoot, "", syscall.MS_BIND|syscall.MS_REC, ""), "pivotRoot(): syscall mount")
+
+	putOld := filepath.Join(newRoot, ".put_old")
+	checkErr(os.MkdirAll(putOld, 0700), "pivotRoot(): mkdirall")
+
+	checkErr(syscall.PivotRoot(newRoot, putOld), "pivotRoot(): pivotRoot")
+	checkErr(syscall.Chdir("/"), "pivotRoot(): chrdir")
+}
+
+func unzipImage(dest, src string) {
+	checkErr(os.MkdirAll(dest, 0700), "unzipImage: mkdirall")
+
+	cmd := exec.Command("tar", []string{"-xzf", src, "-C", dest}...)
+	checkErr(cmd.Run(), "unzipImage: cmd run")
 }
 
 func checkErr(err error, context string) {
